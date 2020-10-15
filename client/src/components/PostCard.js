16d609f34eb737b54185e8f14ad9d4c8ea528c9e
Timofey Bogdanov <tim.the.bogdanov@gmail.com>
2020-10-15 15:04:05 -0700

    set up posts to display on home page, created postCards

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Card, Image, Button } from 'semantic-ui-react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+
+const PostCard = ({
+  post: { body, createdAt, id, username, likeCount, commentCount, likes },
+}) => {
+  const likePost = () => {
+    console.log('like post');
+  };
+
+  const commentOnPost = () => {
+    console.log('comment on post');
+  };
+
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button
+          basic
+          size='tiny'
+          icon='heart'
+          label={{ basic: true, content: likeCount }}
+          labelPosition='right'
+          onClick={likePost}
+        />
+        <Button
+          basic
+          size='tiny'
+          icon='conversation'
+          label={{ basic: true, content: commentCount }}
+          labelPosition='right'
+          onClick={commentOnPost}
+        />
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default PostCard;
