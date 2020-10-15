2e9f0819b96dbb77c955c27b97b4123d2ba63f48
Timofey Bogdanov <tim.the.bogdanov@gmail.com>
2020-10-14 21:30:04 -0700

    add post mutations

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config');
+const { MONGO_DB } = require('./config');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => ({ req }),
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB Connected');
     return server.listen({ port: 5000 });
