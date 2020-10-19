15db9f81d711bff3e463c08ce0f31df21db3b022
Timofey Bogdanov <tim.the.bogdanov@gmail.com>
2020-10-19 15:12:41 -0700

    complete delete post functionality

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
-
 import { Button, Confirm } from 'semantic-ui-react';
+
+import { FETCH_POSTS_QUERY } from '../utils/graphql';
 
 const DeleteButton = ({ postId, callback }) => {
   const [confirmOpen, setConfirmOpen] = useState(false);
 
   const [deletePost] = useMutation(DELETE_POST_MUTATION, {
-    update() {
+    update(proxy) {
       setConfirmOpen(false);
-      // TODO: remove post from cache
+
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY,
+      });
+
+      // data.getPosts = data.getPosts.filter((post) => post.id !== postId);
+      let newData = [...data.getPosts.filter((post) => post.id !== postId)];
+
+      proxy.writeQuery({
+        query: FETCH_POSTS_QUERY,
+        data: {
+          ...data,
+          getPosts: {
+            newData,
+          },
+        },
+      });
+
       if (callback) callback();
     },
     variables: {
