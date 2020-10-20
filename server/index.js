0d0969eb69f747b96f7c16f52a7afb680a0cea30
Timofey Bogdanov <tim.the.bogdanov@gmail.com>
2020-10-19 19:00:52 -0700

    seperate client from server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const { MONGO_DB } = require('./config');
 
 const pubsub = new PubSub();
 
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -17,8 +19,11 @@ mongoose
   .connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });
