83048d9bf1d32d80bf6b775008107115ed8f0a17
Timofey Bogdanov <tim.the.bogdanov@gmail.com>
2020-10-21 08:55:59 -0700

    change address

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://social-network-graphql.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
